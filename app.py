4edd04c7835c158a5ae7b0aece234f29313b46bf
Dewi Lukas <dewilukas89@gmail.com>
2023-12-08 23:20:23 +0800

    init commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb+srv://dewi:Lukas123@dewi.qem00vl.mongodb.net/?retryWrites=true&w=majority")
+db = client.bookprojec
+
+
+app = flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/movie", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
+    }
+
+    
+    # Gunakan requests library untuk mendapatkan kode HTML dari link diatas
+    data = requests.get(url_receive , headers=headers)
+
+    # library BeautifulSoup memudahkan kita dalam
+    # menguraikan kode HTML tersebut,
+    soup = BeautifulSoup(data.text, "html.parser")
+
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[property="og:description"]')
+
+    image = og_image.get("content","tidak ada")
+    title = og_title["content"]
+    desc  = og_description["content"].split("|")
+
+    doc = {
+        'image' : image,
+        'title' : title,
+        'description' : desc,
+        'star' : star_receive,
+        'comment' : comment_receive,
+    }
+    db.movies.insert_one(doc)
+    return jsonify({'msg':'POST request!'})
+
+@app.route("/movie", methods=["GET"])
+def movie_get():
+    movie_list = list(db.movies.find({},{'_id': False}))
+    return jsonify({'movies': movie_list})
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
